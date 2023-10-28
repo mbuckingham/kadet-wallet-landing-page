12630ca19a731a4b8ff5856fff65f85a30172199
andreawork-hub <andrea.vitova@hotmail.com>
2023-10-28 20:09:18 +0200

    changed px to rem

--- src/container/Hero/Hero.jsx
+++ src/container/Hero/Hero.jsx
@@ -1,131 +1,22 @@
 import React from "react";
 import chromeLogo from "../../assets/chrome_logo.png";
-import heroRockets from "../../assets/hero_rockets.png";
 import { ButtonHero } from "../../components";
-import { Wrapper } from "../../components";
+import "./Hero.scss";
 
 const Hero = () => {
   return (
-    <Wrapper className="heroWrapper">
-      <div
-        className="heroImage"
-        style={{
-          display: "flex",
-          height: "45.875rem",
-          justifyContent: "center",
-          alignItems: "center",
-          flex: "1 0 0",
-          backgroundImage: `url(${heroRockets})`,
-          backgroundSize: "contain",
-          backgroundRepeat: "no-repeat",
-        }}
-      ></div>
-      <div
-        className="heroText"
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "flex-start",
-          flex: "1 0 0",
-          alignSelf: "stretch",
-        }}
-      >
-        <h1
-          style={{
-            color: "var(--primary-color-header)",
-            fontFamily: "var(--font-base)",
-            fontSize: "3.56rem",
-            fontStyle: "normal",
-            fontWeight: "700",
-            lineHeight: "120%",
-            letterSpacing: "0.036rem",
-            textTransform: "none",
-            marginTop: "9rem",
-          }}
-        >
-          Navigate the Crypto Universe with Kadet
-        </h1>
-        <p
-          style={{
-            color: "var(--secondary-color-text)",
-            fontFamily: "var(--font-base)",
-            fontSize: "1.44rem",
-            fontStyle: "normal",
-            fontWeight: "500",
-            lineHeight: "120%",
-            letterSpacing: "0.014rem",
-            textTransform: "none",
-            marginTop: "1.6rem",
-          }}
-        >
+    <div className="wrapper-hero">
+      <div className="heroImage"></div>
+      <div className="heroText">
+        <h1>Navigate the Crypto Universe with Kadet</h1>
+        <p>
           Explore the the world of crypto with Kadet Wallet! The solution to all
           of your cross-chain management, storage, and transaction needs.
         </p>
         <ButtonHero icon={chromeLogo}>Download Extention</ButtonHero>
       </div>
-    </Wrapper>
+    </div>
   );
 };
 
 export default Hero;
-
-/*
-  <img src={chromeLogo} alt="Chrome Icon" />
-const [hero_section, setHero_section] = useState([]);
-
-useEffect(() => {
-  const query = '*[_type == "hero_section"]';
-
-  client
-    .fetch(query)
-  .then((data) => setHero_section(data))
-   .catch((err) => console.error(err));
-}, []);
-<div className="app__header app__flex">
-      { Mockup! }
-      <motion.div
-        whileInView={{ opacity: [0, 1] }}
-        transition={{ duration: 0.5, delayChildren: 0.5 }}
-        className="app__header-img"
-      >
-        { <img
-          src={
-            hero_section?.imgUrl
-              ? urlFor(hero_section.imgUrl)
-              : images.fallback_image
-          }
-          alt={hero_section?.title || "Fallback Title"}
-        /> }
-        <img src={images.create_account_hero} alt="mockup" />
-        <motion.img
-          whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-          transition={{ duration: 0.5 }}
-        />
-      </motion.div>
-
-      <motion.div
-        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-        transition={{ duration: 0.5 }}
-        className="app__header-info"
-      >
-        <div className="app__header-badge">
-          <div className="badge-cmp app__flex">
-            <div style={{ marginLeft: 20 }}>
-              <h1 className="head-text">
-                The first cross-chain crypto wallet on Kadena
-              </h1>
-              <div style={{ marginTop: 20, marginBottom: 80 }}>
-                <p className="p-text">
-                  In addition to sending, receiving, and importing tokens, Kadet
-                  Wallet simplifies cross-chain transfers with our new Autochain
-                  feature
-                </p>
-              </div>
-              <Button />
-            </div>
-          </div>
-        </div>
-      </motion.div>
-    </div>
-
-*/
--- src/container/KadetMission/KadetMission.jsx
+++ src/container/KadetMission/KadetMission.jsx
@@ -1,134 +1,22 @@
 import React from "react";
-import { ButtonMission, Wrapper } from "../../components";
-import kadetMission from "../../assets/planet_tokens.png";
+import { ButtonMission } from "../../components";
+import "./KadetMission.scss";
 import arrowDark from "../../assets/arrow_dark.png";
 
 const KadetMission = () => {
   return (
-    <Wrapper className="wrapper">
-      <div
-        className="KUText"
-        style={{
-          //border: "2px solid green",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "flex-start",
-          flex: "1 0 0",
-          alignSelf: "stretch",
-        }}
-      >
-        <h1
-          className="missionHeader"
-          style={{
-            color: "var(--primary-color-header)",
-            fontFamily: "var(--font-base)",
-            fontSize: "3.56rem",
-            fontStyle: "normal",
-            fontWeight: "700",
-            lineHeight: "120%",
-            letterSpacing: "0.036rem",
-            textTransform: "none",
-            marginTop: "6rem",
-          }}
-        >
-          The Kadet Mission
-        </h1>
-        <p
-          className="missionText"
-          style={{
-            color: "var(--secondary-color-text)",
-            fontFamily: "var(--font-base)",
-            fontSize: "1.44rem",
-            fontStyle: "normal",
-            fontWeight: "500",
-            lineHeight: "120%",
-            letterSpacing: "0.014rem",
-            textTransform: "none",
-            marginTop: "1.5rem",
-          }}
-        >
+    <div className="wrapper-mission">
+      <div className="KUText">
+        <h1>The Kadet Mission</h1>
+        <p>
           Kadet Wallet was designed with beginners in mind. To make your
           entrance into the world of digital finance simple and easy.
         </p>
         <ButtonMission icon={arrowDark}>Start Learning</ButtonMission>
       </div>
-      <div
-        className="missionImage"
-        style={{
-          //border: "2px solid blue",
-          display: "flex",
-          height: "32.5rem",
-          justifyContent: "center",
-          alignItems: "center",
-          flex: "1 0 0",
-          backgroundImage: `url(${kadetMission})`,
-          backgroundSize: "contain",
-          backgroundRepeat: "no-repeat",
-        }}
-      ></div>
-    </Wrapper>
-  );
-};
-
-export default KadetMission;
-
-/** const [hero_section, setHero_section] = useState([]);
-
-  useEffect(() => {
-    const query = '*[_type == "hero_section"]';
-
-    client
-      .fetch(query)
-      .then((data) => setHero_section(data))
-      .catch((err) => console.error(err));
-  }, []); 
-
-  return (
-    <div className="app__kadet_university app__flex">
-      {/* kadet university content }
-      <motion.div
-        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-        transition={{ duration: 0.5 }}
-        className="app__kadet_university-info"
-      >
-        <div className="app__kadet_university-badge">
-          <div className="badge-cmp app__flex">
-            <div style={{ marginLeft: 20 }}>
-              <h1 className="head-text">Kadet University</h1>
-              <div style={{ marginTop: 20, marginBottom: 80 }}>
-                <p className="p-text">
-                  From Crypto 101 to your smashout predictions for 2023 and
-                  beyond.
-                </p>
-              </div>
-              <ButtonUniversity />
-            </div>
-          </div>
-        </div>
-      </motion.div>
-
-      {/* Mockup! }
-      <motion.div
-        whileInView={{ opacity: [0, 1] }}
-        transition={{ duration: 0.5, delayChildren: 0.5 }}
-        className="app__kadet_university-img"
-      >
-        {/* <img
-          src={
-            hero_section?.imgUrl
-              ? urlFor(hero_section.imgUrl)
-              : images.fallback_image
-          }
-          alt={hero_section?.title || "Fallback Title"}
-        /> 
-        {/*  <img src={images.planet_university} alt="mockup" /> }
-        <motion.img
-          whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-          transition={{ duration: 0.5 }}
-        />
-      </motion.div>
+      <div className="missionImage"></div>
     </div>
   );
 };
 
-export default KadetMission;*/
+export default KadetMission;
